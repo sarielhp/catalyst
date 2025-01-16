bed49c9282de77609e50bcdc14a6ffc6f53b0e3e
Stav <stava2@illinois.edu>
2025-01-16 10:25:24 -0600

    debugged never ending rrt

--- cat_exp/run_pmpl.py
+++ cat_exp/run_pmpl.py
@@ -8,10 +8,27 @@ def generate_random_int(start, end):
     return random.randint(start, end)
 
 def replace_placeholders(content):
+
     content = content.replace("@@seed@@", str(generate_random_int(0, 9999999)))
+
+    start_ranges = [[-3.0, 3.0], [-3.0, 3.0], [-3.0, -3.0], [-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0]]
+    goal_ranges = [[-3.0, 3.0], [-3.0, 3.0], [3.0, 3.0], [-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0]]
+
+    content = content.replace("@@start@@", generate_numbers_from_ranges(start_ranges))
+    content = content.replace("@@goal@@", generate_numbers_from_ranges(goal_ranges))
 
     return content
 
+def generate_numbers_from_ranges(ranges):
+    result = []
+    for r in ranges:
+        min_val, max_val = r
+        if min_val <= max_val:
+            number = generate_random_float(min_val, max_val)
+            result.append(str(number))
+        else:
+            raise ValueError("Invalid range: min value is greater than max value")
+    return ' '.join(result)
 def main():
     with open("CatExperimentTemplate.xml", "r") as template_file:
         template_content = template_file.read()
