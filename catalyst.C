9e01032eac6d0684bb3feaca6ea233612026a333
Sariel Har-Peled <sariel@red.sarielhp.org>
2025-01-27 21:58:18 -0600

    Time/date: 01_27_25__21_58_18 from red

--- catalyst.C
+++ catalyst.C
@@ -829,7 +829,8 @@ void  SManager::spawn_single_task()
     Task  * tsk = new  Task();
 
     counter_tasks_created++;
-
+    printf( "# Task created: %d\n", counter_tasks_created );
+    
     // We run a process for one second if we are in the wide search mode...
     if  ( f_wide_search ) {
         tsk->set_id( counter_tasks_created );
