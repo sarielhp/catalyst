a1d7cac3f0c43e32e59db8554fd3e36d49269b15
Sariel Har-Peled <sariel@gmail.com>
2025-01-29 19:22:12 -0600

    Time/date: 01_29_25__19_22_12 from valis

--- julia/stats.jl
+++ julia/stats.jl
@@ -6,14 +6,19 @@ function  (@main)(ARGS)
     arr = open( ARGS[1] ) do f
         readlines(f) |> (s->parse.(Float64, s))
     end
+    if length( arr ) == 0
+        exit( -1 );
+    end
 #    println( arr );
     println( "" );
     println( "Filename : ", ARGS[ 1 ] );
     println( "#        : ", length(arr) );
 #    println( typeof( arr ) );
     println( "Mean     : ", mean( arr ) );
-    println( "Median   : ", median( arr ) );
-    println( "StdDev   : ", Statistics.std( arr ) );
+    if  ( length( arr ) > 1 ) 
+        println( "Median   : ", median( arr ) );
+        println( "StdDev   : ", Statistics.std( arr ) );
+    end
     println( "Minimum  : ", minimum( arr ) );
     println( "Maximum  : ", maximum( arr ) );
 #    println( "Variance : ", Statistics.var( arr ) );
